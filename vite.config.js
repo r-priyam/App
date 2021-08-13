47444b85785b36088309f8a8c0bd62b3078d9186
priyam <priyamroy29@gmail.com>
2021-08-13 17:07:58 +0530

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [
-    vue({
-      script: {
-        refSugar: true,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    host: true,
-    open: true,
-    port: 8000,
-    strictPort: true,
-  },
+	plugins: [
+		vue({
+			script: {
+				refSugar: true,
+			},
+		}),
+	],
+	server: {
+		host: true,
+		open: true,
+		port: 8000,
+		strictPort: true,
+	},
 });
