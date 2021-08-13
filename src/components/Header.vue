aca39ce4b5f1dc62797ff376ff391c9601d55d14
priyam <priyamroy29@gmail.com>
2021-08-14 01:46:59 +0530

    logo update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@
             <span class="sr-only">Workflow</span>
             <img
               class="h-8 w-auto sm:h-10"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              src="../assets/favicon.svg"
               alt
             />
           </a>
